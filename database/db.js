520636f055e382b584ce9a168c538907e53a4b10
sylveste <nora.sylveste@yahoo.com>
2026-06-21 19:06:15 +0200

    suppression du fichier commentaire.db present a la racine

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 
 
-const db = new sqlite3.Database("./commentaires.db",(err)=>{
+const db = new sqlite3.Database("./database/commentaires.db",(err)=>{
 
  if(err){
     console.log(err.message)
